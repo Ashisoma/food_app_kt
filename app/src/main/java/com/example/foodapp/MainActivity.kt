2c8cd3d01a1122bdf1f25ee90e3b019ab8de824c
Ashisoma <okalionesmus@gmail.com>
2022-08-15 14:33:30 +0300

    initial kt commit

---
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.foodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.btn_nav)
+
+        val navCTRL = Navigation.findNavController(this, R.id.fragments)
+
+        NavigationUI.setupWithNavController(bottomNav,navCTRL)
+    }
+}
